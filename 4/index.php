fcd0067c95c90bb279f748bfd10e662fd2faeff5
Krillof <61355245+Krillof@users.noreply.github.com>
2023-05-02 11:48:31 +0300

    1

--- 4/index.php
+++ 4/index.php
@@ -10,15 +10,6 @@ if (!isset($included)){
   $included = True;
 }
 
-
-function send_error_and_exit($error_message, $error_code="400"){
-  header("HTTP/1.1 " . $error_code . " " . $error_message);
-  exit();
-}
-
-$user = 'u53304';
-$pass = '1449484';
-$db = new PDO('mysql:host=localhost;dbname='.$user, $user, $pass, [PDO::ATTR_PERSISTENT => true]);
 $columns = array();
 $columns[] = 'full_name';
 $columns[] = 'email';
